51fe30fbaa13cf391bbce817fe10838f595f6287
dili <muhammaddilia99@gamil.com>
2026-05-01 16:00:47 +0700

    fix:catchasync function

--- services/bookingService/src/utils/catchAsync.js
+++ services/bookingService/src/utils/catchAsync.js
@@ -1,6 +1,6 @@
 const catchAsync = (controller) => async (req, res, next) => {
   try {
-    await controller(req, res);
+    await controller(req, res, next);
   } catch (error) {
     next(error);
   }
